05f6f5a22bc909e710a1636168489e1de65cbfe9
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-07-22 05:42:19 +0300

    Assertion.Core project

---
+++ src/FlueFlame.Extensions.Assertions.Core/ResponseModuleWithAssertions.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using FlueFlame.AspNetCore;
+using FlueFlame.AspNetCore.Modules.Response;
+
+namespace FlueFlame.Extensions.Assertions.Core;
+
+public abstract class ResponseModuleWithAssertions : ResponseModule
+{
+    public ResponseModuleWithAssertions(FlueFlameHost application) : base(application)
+    {
+        
+    }
+
+    #region Status Code
+
+    public abstract ResponseModule AssertStatusCode(HttpStatusCode statusCode);
+
+    public abstract ResponseModule AssertStatusCode(int statusCode);
+
+    #endregion
+    
+    #region Headers
+
+    public abstract ResponseModule AssertContainsHeaders(params string[] headers);
+
+    public abstract ResponseModule AssertDoesNotContainsHeaders(params string[] headers);
+
+    public abstract ResponseModule AssertHeader(string key, string value);
+
+    public abstract ResponseModule AssertHeaderPattern(string key, string pattern);
+
+    #endregion
+
+    public abstract ResponseModule AssertBodyLength(int length);
+}
