d5b1cc1309a689f05f408abb529e4e5e6294dd4a
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-11-26 16:51:38 +0300

    Default headers for HttpFacade.cs

--- src/FlueFlame.AspNetCore/Facades/HttpFacade.cs
+++ src/FlueFlame.AspNetCore/Facades/HttpFacade.cs
@@ -1,14 +1,17 @@
 ﻿using System.Net.Http;
 using FlueFlame.AspNetCore.Common;
 using FlueFlame.AspNetCore.Modules.Http;
+using FlueFlame.AspNetCore.Services;
+using Microsoft.Extensions.Primitives;
 
 namespace FlueFlame.AspNetCore.Facades
 {
     public class HttpFacade : FlueFlameFacadeBase
     {
-        public HttpFacade(IFlueFlameHost application) : base(application)
+        private HttpService HttpService;
+        internal HttpFacade(IFlueFlameHost application, HttpService httpService) : base(application)
         {
-            
+            HttpService = httpService;
         }
 
 
@@ -20,5 +23,38 @@ namespace FlueFlame.AspNetCore.Facades
         public HttpModule Post => new(Application, HttpMethod.Post);
         public HttpModule Put => new(Application, HttpMethod.Put);
         public HttpModule Trace => new(Application, HttpMethod.Trace);
+
+
+        public HttpFacade AddDefaultHeader(string key, string value)
+        {
+            HttpService.ConfigureHttpContextPermanently(context =>
+            {
+                context.Request.Headers[key] = value;
+            });
+            return this;
+        }
+        
+        public HttpFacade AddDefaultHeader(string key, StringValues values)
+        {
+            HttpService.ConfigureHttpContextPermanently(context =>
+            {
+                context.Request.Headers[key] = values;
+            });
+            return this;
+        }
+        
+        public HttpFacade AddDefaultBearerToken(string token)
+        {
+            token = token.Replace("Bearer ", "");
+            HttpService.ConfigureHttpContextPermanently(x => x.Request.Headers["Authorization"] = $"Bearer {token}");
+            return this;
+        }
+
+        public HttpFacade Reset()
+        {
+            HttpService.Reset();
+            HttpService.ResetPermanently();
+            return this;
+        }
     }
 }
