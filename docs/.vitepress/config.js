ffd35305648be7cd5d93589c4b36d0f0fc0309a4
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-11-27 00:58:47 +0300

    Initial vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,43 @@
+export default {
+    title: 'FlueFlame',
+    description: 'Fluent testing',
+    themeConfig: {
+        logo: '/FlueFlameLogo.svg',
+        sidebar: [
+            {
+                text: 'Introduction',
+                collapsible: true,
+                items: [
+                    { text: 'Introduction', link: '/introduction/intro.md' }
+                ]
+            },
+            {
+                text: 'REST',
+                collapsible: true,
+                items: [
+                    { text: 'Basics', link: '/rest/basic.md' }
+                ]
+            },
+            {
+                text: 'SignalR',
+                collapsible: true,
+                items: [
+                    { text: 'Basics', link: '/signalr/basic.md' }
+                ]
+            },
+            {
+                text: 'gRPC',
+                collapsible: true,
+                items: [
+                    { text: 'Basics', link: '/grpc/basic.md' }
+                ]
+            }
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/ISBronny/FlueFlame' }
+        ],
+        editLink: {
+            pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path'
+        }
+    }
+}
