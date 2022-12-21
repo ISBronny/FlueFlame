3009458f760161064df21816e3beb0270d1e711d
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-12-22 01:52:24 +0300

    Grpc test examples

--- src/Examples.Tests.Api/Grpc/EmployeeServiceTests.cs
+++ src/Examples.Tests.Api/Grpc/EmployeeServiceTests.cs
@@ -16,96 +16,86 @@ public class EmployeeServiceTests : IntegrationTestBase
 	{
 		var employee = new EmployeeTestDataBuilder(EmployeeContext)
 			.Build();
-	
+
 		GrpcHost
 			.CreateConnection<EmployeeService.EmployeeServiceClient>()
-			.UnaryRpc(
-				client => client
-					.GetById(employee.Guid.ToGrpcModel())
-						.Guid.Should().Be(employee.Guid.ToString())
-				);
+			.Unary
+				.Call(c => c.GetById(employee.Guid.ToGrpcModel()))
+				.Response
+					.AssertThat(e => e.Guid.Should().Be(employee.Guid.ToString()));
 	}
 	
 	[Fact]
-	public void GetByIdTest_DoesNotExists_Throws()
+	public void GetByIdTest_DoesNotExists_ReturnNotFound()
 	{
 		GrpcHost
 			.CreateConnection<EmployeeService.EmployeeServiceClient>()
-			.UnaryRpc(
-				client => FluentActions.Invoking(() => client.GetById(RandomGuidRequest))
-					.Should().Throw<RpcException>().Which.StatusCode.Should().Be(StatusCode.NotFound)
-			);
+			.Unary
+				.Call(c => c.GetById(RandomGuidRequest))
+				.Response
+					.AssertStatusCode(StatusCode.NotFound);
 	}
-	
+
 	[Fact]
 	public void CreateEmployees_ValidEmployees_ReturnsIds()
 	{
 		var employees = Enumerable.Range(0, 10)
-			.Select(_ => new EmployeeTestDataBuilder().Build(saveInDb: false))
-			.ToList();
+				.Select(_ => new EmployeeTestDataBuilder().Build(saveInDb: false))
+				.Select(e=>e.ToGrpcModel())
+		 		.ToList();
 		
 		GrpcHost
 			.CreateConnection<EmployeeService.EmployeeServiceClient>()
-			.ClientStreaming(
-				client => client.CreateEmployees(),
-				async call =>
-				{
-					foreach (var e in employees)
-						await call.RequestStream.WriteAsync(e.ToGrpcModel());
-					await call.RequestStream.CompleteAsync();
-				},
-				response => response.Guid.Should().BeEquivalentTo(employees.Select(e => e.Guid.ToString()))
-			);
+			.ClientStreaming
+				.Call(x=>x.CreateEmployees())
+				.RequestStream
+					.WriteMany(employees)
+					.Complete()
+				.Response
+					.AssertThat(resp=>resp.Guid.Should().BeEquivalentTo(employees.Select(x=>x.Guid)));
 	}
+
 	
 	[Fact]
-	public void CreateEmployees_AlreadyExists_ReturnsIds()
-	{
-		var employees = Enumerable.Range(0, 10)
-			.Select(i => new EmployeeTestDataBuilder(EmployeeContext).Build(saveInDb: i > 5))
-			.ToList();
-		
-		GrpcHost
-			.CreateConnection<EmployeeService.EmployeeServiceClient>()
-			.ClientStreaming(
-				client => client.CreateEmployees(),
-				async call =>
-				{
-					var t = await FluentActions.Awaiting(async () =>
-					{
-						foreach (var e in employees)
-							await call.RequestStream.WriteAsync(e.ToGrpcModel());
-						await call.RequestStream.CompleteAsync();
-					}).Should().ThrowAsync<RpcException>();
-					t.Which.StatusCode.Should().Be(StatusCode.AlreadyExists);
-				}
-			);
-	}
-	
-	[Fact]
-	public void GetByIdTest_DoesNotExists_ReturnsEmployee2()
+	public void CreateEmployees_AlreadyExists_ReturnsAlreadyExists()
 	{
 		var employee = new EmployeeTestDataBuilder(EmployeeContext)
-			.Build();
+			.Build(saveInDb: true);
 		
 		GrpcHost
 			.CreateConnection<EmployeeService.EmployeeServiceClient>()
-			.BidirectionalStreaming(
-				client => client.GetByIds(),
-				async duplex =>
-				{
-					await duplex.RequestStream.WriteAsync(employee.Guid.ToGrpcModel());
-					await duplex.RequestStream.WriteAsync(RandomGuidRequest);
-					await duplex.RequestStream.CompleteAsync();
-					await duplex.ResponseStream.MoveNext();
-					duplex.ResponseStream.Current.Guid.Should().Be(employee.Guid.ToString());
-					await FluentActions.Awaiting(async () => await duplex.ResponseStream.MoveNext())
-						.Should()
-						.ThrowAsync<RpcException>();
-
-				}
-			);
+			.ClientStreaming
+				.Call(x=>x.CreateEmployees())
+				.RequestStream
+					.Write(employee.ToGrpcModel())
+				.Response
+					.AssertStatusCode(StatusCode.AlreadyExists);
 	}
+	//
+	// [Fact]
+	// public void GetByIdTest_DoesNotExists_ReturnsEmployee2()
+	// {
+	// 	var employee = new EmployeeTestDataBuilder(EmployeeContext)
+	// 		.Build();
+	// 	
+	// 	GrpcHost
+	// 		.CreateConnection<EmployeeService.EmployeeServiceClient>()
+	// 		.BidirectionalStreaming(
+	// 			client => client.GetByIds(),
+	// 			async duplex =>
+	// 			{
+	// 				await duplex.RequestStream.WriteAsync(employee.Guid.ToGrpcModel());
+	// 				await duplex.RequestStream.WriteAsync(RandomGuidRequest);
+	// 				await duplex.RequestStream.CompleteAsync();
+	// 				await duplex.ResponseStream.MoveNext();
+	// 				duplex.ResponseStream.Current.Guid.Should().Be(employee.Guid.ToString());
+	// 				await FluentActions.Awaiting(async () => await duplex.ResponseStream.MoveNext())
+	// 					.Should()
+	// 					.ThrowAsync<RpcException>();
+	//
+	// 			}
+	// 		);
+	// }
 }
 
 public static class GrpcExtensions
